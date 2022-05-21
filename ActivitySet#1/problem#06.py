9104644c2c6f98414788b65e4ed027174c8b4dd3
kush46 <97089328+kushal46@users.noreply.github.com>
2022-05-21 07:39:49 +0000

    fresh

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -2,15 +2,19 @@
 
 largest = None
 smallest = None
-
 while True:
-    num = input("Enter a number? ")
+  try:
+      num = input("Enter a number: ")
+      if num == "done":
+            break
+      n = int(num)
+      if largest is None or n > largest:
+            largest = n
+      elif smallest is None or n < smallest:
+            smallest = n
+  except ValueError:
+      print("Invalid input")
+      continue
 
-    if num == "done":
-        break
-
-    # ...
-
-    print(num)
-
-print("Maximum", largest)
+print ("Maximum is", largest)
+print ("Minimum is", smallest)
