d29967087f5a38f93c07cb11ba9ea7c04c59d39d
kush46 <97089328+kushal46@users.noreply.github.com>
2022-05-27 04:58:05 +0000

    new

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,7 +1,7 @@
 # Strings
 
-text = "X-DSPAM-Confidence:    0.8475"
-i = text.find('0.8475')
-s = slice(i,50)
-ans = float(text[s])
-print(ans)
+ text = "X-DSPAM-Confidence:    0.8475"
+ i = text.find('0.8475')
+ s = slice(i,50)
+ ans = float(text[s])
+ print(ans)
